4480fef2fd493d89e1f72cac17ce32e81cac4c0b
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-02-27 15:50:52 -0500

    minor change contact sensor

--- scripts/demos/sensors/contact_sensor_spot_sphere.py
+++ scripts/demos/sensors/contact_sensor_spot_sphere.py
@@ -78,17 +78,18 @@ class ContactSensorSceneCfg(InteractiveSceneCfg):
         init_state=RigidObjectCfg.InitialStateCfg(pos=(1, 1, 0.05)),
     )
 
+    # Rigid Object
     cube = RigidObjectCfg(
         prim_path="{ENV_REGEX_NS}/Cube",
         spawn=sim_utils.CuboidCfg(
-            size=(0.3, 0.3, 0.1),
+            size=(0.5, 0.5, 0.1),
             rigid_props=sim_utils.RigidBodyPropertiesCfg(),
             mass_props=sim_utils.MassPropertiesCfg(mass=100.0),
             collision_props=sim_utils.CollisionPropertiesCfg(),
             physics_material=sim_utils.RigidBodyMaterialCfg(static_friction=1.0),
             visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0), metallic=0.2),
         ),
-        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.25, 0.25, 0.05)),
+        init_state=RigidObjectCfg.InitialStateCfg(pos=(0.5, 0.5, 0.05)),
     )
 
     contact_forces_LF = ContactSensorCfg(
