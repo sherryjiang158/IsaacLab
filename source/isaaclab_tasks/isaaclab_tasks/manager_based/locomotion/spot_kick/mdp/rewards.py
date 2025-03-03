3834f16279f700d3b95f00b9ba9e2c62c55c8315
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-03 13:15:50 -0500

    spot kick v0 small change

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/rewards.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/rewards.py
@@ -11,25 +11,6 @@ if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
     from isaaclab.managers import RewardTermCfg
 
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-from __future__ import annotations
-
-import torch
-from typing import TYPE_CHECKING
-
-from isaaclab.managers import SceneEntityCfg
-from isaaclab.utils.math import matrix_from_quat
-
-if TYPE_CHECKING:
-    from isaaclab.envs import ManagerBasedRLEnv
-
-
-import torch
-
 def approach_ball(env, params):
     """
     Reward for approaching the ball using an inverse-square law.
