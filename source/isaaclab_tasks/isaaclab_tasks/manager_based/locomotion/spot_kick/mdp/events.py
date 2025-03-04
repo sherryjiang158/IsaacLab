482dcdbce9bf7fdfb2c85386dcbc415300812dbe
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-03 22:59:29 -0500

    spot kick v0 small change

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/events.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/events.py
@@ -129,6 +129,8 @@ def randomize_ball_position(env, position_range: tuple = None) -> None:
     
     # Define a base offset, e.g., place the ball 0.1 m in front of the toe.
     base_offset = torch.tensor([0.1, 0.0, 0.0], device=toe_pos.device).unsqueeze(0)  # shape: [1, 3]
+    position_range = ((-0.1, 0.1), (-0.1, 0.1), (0.0, 0.0))
+
     
     # Determine random offset if position_range is provided.
     if position_range is not None:
