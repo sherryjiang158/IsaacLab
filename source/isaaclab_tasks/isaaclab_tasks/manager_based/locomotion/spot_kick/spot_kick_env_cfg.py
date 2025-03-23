65b0b4c0e5a7d056c02001c3ce464152fc4819e4
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-23 18:35:21 -0400

    add printing for termination

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/spot_kick_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/spot_kick_env_cfg.py
@@ -377,7 +377,7 @@ class TerminationsCfg:
     """Termination terms for the MDP."""
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
     body_contact = DoneTerm(
-        func=mdp.illegal_contact,
+        func=mdp.illegal_contact_kick,
         params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=["body", ".*leg"]), "threshold": 1.0},
     )
 
