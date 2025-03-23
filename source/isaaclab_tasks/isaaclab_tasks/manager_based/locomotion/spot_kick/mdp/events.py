1aeee7018fe71111fe1712b81b1b24f3302bcc30
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-23 19:31:54 -0400

    debug update ball position

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/events.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/events.py
@@ -81,7 +81,7 @@ def randomize_ball_position(env, position_range: tuple = None) -> None:
     
     # Define a base offset, e.g., place the ball 0.1 m in front of the toe.
     base_offset = torch.tensor([0.1, 0, 0], device=toe_pos.device).unsqueeze(0)  # shape: [1, 3]
-    position_range = ((-0.05, 0.05), (-0.05, 0.05), (0.0, 0.0))
+    position_range = ((0.0, 0.0), (0.0, 0.0), (0.0, 0.0))
     # print("!!!! Randomized ball pos is run. Toe position:", toe_pos)
     
     # Determine random offset if position_range is provided.
