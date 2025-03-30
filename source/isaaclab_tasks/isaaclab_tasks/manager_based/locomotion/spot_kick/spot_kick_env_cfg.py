8dc8a96fd2f7d251c9b3f63248510b2ebf3061f2
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-30 13:30:54 -0400

    edit/remove randomize ball position

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/spot_kick_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/spot_kick_env_cfg.py
@@ -272,13 +272,13 @@ class EventCfg:
         },
     )
      # Randomize the ball's starting position for additional variability
-    randomize_ball_position = EventTerm(
-        func=mdp.randomize_ball_position,
-        mode="reset",
-        # params={
-        #     "position_range": ((-0.1, 0.1), (-0.1, 0.1), (0.0, 0.0)), # !!!! May need to adjust based on code running situation
-        # },
-    )
+    # randomize_ball_position = EventTerm(
+    #     func=mdp.randomize_ball_position,
+    #     mode="reset",
+    #     # params={
+    #     #     "position_range": ((-0.1, 0.1), (-0.1, 0.1), (0.0, 0.0)), # !!!! May need to adjust based on code running situation
+    #     # },
+    # )
 
     
 @configclass
