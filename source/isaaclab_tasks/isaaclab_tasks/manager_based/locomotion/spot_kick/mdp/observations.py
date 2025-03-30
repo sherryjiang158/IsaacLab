c580ff36e3ffc74f0dd4eec66771df2af1b0b274
Sherry Jiang <sherryjiang@host-SherrysaceBlack-mac-80a9974a7a0b.eduroam.ssid.10net.amherst.edu>
2025-03-30 13:49:10 -0400

    edit/remove randomize ball position

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/observations.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/spot_kick/mdp/observations.py
@@ -19,9 +19,10 @@ def rel_ball_leg_position(env: ManagerBasedRLEnv) -> torch.Tensor:
     # Get the frame data for the kicking leg (toe)
     leg_tf_data = env.scene["kicking_leg_frame"].data
     print("kicking leg frame", leg_tf_data.target_pos_w[..., 0, :])
-    
+
     # Get the ball's data
     ball_data = env.scene["ball"].data
+    print("ball pos", ball_data.root_pos_w)
 
     # Compute the vector difference: ball position minus toe position.
     # [# envs, which target frame, : all coordinates]
